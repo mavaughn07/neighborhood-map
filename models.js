cb707741d90ed9549c13d4c65daee685846e3125
Maxwell Vaughn <mavaughn07@gmail.com>
2018-11-22 15:48:40 -0500

    position should be named location

--- models.js
+++ models.js
@@ -1,127 +1,127 @@
 Views = {
     var bars = [{
         title: "Canton Brew Works",
-        position: {
+        location: {
             lat: 42.348954,
             lng: -83.461236
         }
     }, {
         title: "Liberty Street Brewing",
-        position: {
+        location: {
             lat: 42.379217,
             lng: -83.461315
         }
     }, {
         title: "ROAK Brewing",
-        position: {
+        location: {
             lat: 42.482954,
             lng: -83.140733
         }
     }, {
         title: "Rochester Mills Beer Co",
-        position: {
+        location: {
             lat: 42.689468,
             lng: -83.246000
         }
     }, {
         title: "Brew Detroit",
-        position: {
+        location: {
             lat: 42.327647,
             lng: -83.061788
         }
     }, {
         title: "Detroit Beer Company",
-        position: {
+        location: {
             lat: 42.335982,
             lng: -83.048837
         }
     }, {
         title: "Atwater Brewery",
-        position: {
+        location: {
             lat: 42.337356,
             lng: -83.018481
         }
     }, {
         title: "Motor City Brewing",
-        position: {
+        location: {
             lat: 42.351679,
             lng: -83.065960
         }
     }, {
         title: "Blue Tractor Brewery",
-        position: {
+        location: {
             lat: 42.280641,
             lng: -83.746940
         }
     }, {
         title: "Jolly Pumpkin",
-        position: {
+        location: {
             lat: 42.279148,
             lng: -83.748238
         }
     }, {
         title: "Arbor Brewing",
-        position: {
+        location: {
             lat: 42.280296,
             lng: -83.747845
         }
     }, {
         title: "Glass House",
-        position: {
+        location: {
             lat: 42.274091,
             lng: -83.779037
         }
     }, {
         title: "Wolverine State Brewing Co",
-        position: {
+        location: {
             lat: 42.269681,
             lng: -83.774472
         }
     }, {
         title: "HOMES Brewery",
-        position: {
+        location: {
             lat: 42.269681,
             lng: -83.774472
         }
     }, {
         title: "Grizzly Peak Brewing Co",
-        position: {
+        location: {
             lat: 42.280716,
             lng: -83.749565
         }
     }, {
         title: "Stony Lake Brewing Co",
-        position: {
+        location: {
             lat: 42.170798,
             lng: -83.772459
         }
     }, {
         title: "The Corner Brewery",
-        position: {
+        location: {
             lat: 42.250253,
             lng: -83.609981
         }
     }, {
         title: "Hopcat",
-        position: {
+        location: {
             lat: 42.278930,
             lng: -83.741635
         }
     }, {
         title: "Salt Springs Brewery",
-        position: {
+        location: {
             lat: 42.165879,
             lng: -83.780447
         }
     }, {
         title: "Batch Brewing Company",
-        position: {
+        location: {
             lat: 42.328527,
             lng: -83.063352
         }
     }, {
         title: "Witch's Hat Brewing ",
-        position: {
+        location: {
             lat: 42.454514,
             lng: -83.650345
         }
