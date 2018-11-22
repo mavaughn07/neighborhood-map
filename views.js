bfaf36ca957dd46970c2b2f1e7516039a7cade9e
Maxwell Vaughn <mavaughn07@gmail.com>
2018-11-22 00:22:25 -0500

    created inititial list of breweries

---
+++ views.js
@@ -0,0 +1,43 @@
+Views = {
+    var bars = [{
+        name: "Canton Brew Works"
+    }, {
+        name: "Liberty Street Brewing"
+    }, {
+        name: "Rusty Nail"
+    }, {
+        name: "Herman's"
+    }, {
+        name: "BWW"
+    }, {
+        name: "Sands"
+    }, {
+        name: "Michno's Cafe"
+    }, {
+        name: "Basement Burger Bar"
+    }, {
+        name: "Bailey's"
+    }, {
+        name: "Jolly Pumpkin"
+    } {
+        name: "Arbor Brewing"
+    }, {
+        name: "Knights"
+    }, {
+        name: "Wolverine"
+    }, {
+        name: "Homes"
+    }, {
+        name: "Grizzly Peak"
+    }, {
+        name: "The Grotto"
+    }, {
+        name: "The Corner Brewery"
+    }, {
+        name: "Hopcat"
+    }, {
+        name: "The Last Word"
+    }, {
+        name: "Batch"
+    }]
+}
