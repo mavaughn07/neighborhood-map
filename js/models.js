5f1b80e7142eccce1b77dd276d37eafe0903c767
Maxwell Vaughn <mavaughn07@gmail.com>
2018-11-22 16:00:19 -0500

    created js folder and MVC files

---
+++ js/models.js
@@ -0,0 +1,121 @@
+var locations = [{
+    title: "Canton Brew Works",
+    location: {
+        lat: 42.348954,
+        lng: -83.461236
+    }
+}, {
+    title: "Liberty Street Brewing",
+    location: {
+        lat: 42.379217,
+        lng: -83.461315
+    }
+}, {
+    title: "ROAK Brewing",
+    location: {
+        lat: 42.482954,
+        lng: -83.140733
+    }
+}, {
+    title: "Brew Detroit",
+    location: {
+        lat: 42.327647,
+        lng: -83.061788
+    }
+}, {
+    title: "Detroit Beer Company",
+    location: {
+        lat: 42.335982,
+        lng: -83.048837
+    }
+}, {
+    title: "Atwater Brewery",
+    location: {
+        lat: 42.337356,
+        lng: -83.018481
+    }
+}, {
+    title: "Motor City Brewing",
+    location: {
+        lat: 42.351679,
+        lng: -83.065960
+    }
+}, {
+    title: "Blue Tractor Brewery",
+    location: {
+        lat: 42.280641,
+        lng: -83.746940
+    }
+}, {
+    title: "Jolly Pumpkin",
+    location: {
+        lat: 42.279148,
+        lng: -83.748238
+    }
+}, {
+    title: "Arbor Brewing",
+    location: {
+        lat: 42.280296,
+        lng: -83.747845
+    }
+}, {
+    title: "Glass House",
+    location: {
+        lat: 42.274091,
+        lng: -83.779037
+    }
+}, {
+    title: "Wolverine State Brewing Co",
+    location: {
+        lat: 42.269681,
+        lng: -83.774472
+    }
+}, {
+    title: "HOMES Brewery",
+    location: {
+        lat: 42.280273,
+        lng: -83.778457
+    }
+}, {
+    title: "Grizzly Peak Brewing Co",
+    location: {
+        lat: 42.280716,
+        lng: -83.749565
+    }
+}, {
+    title: "Stony Lake Brewing Co",
+    location: {
+        lat: 42.170798,
+        lng: -83.772459
+    }
+}, {
+    title: "The Corner Brewery",
+    location: {
+        lat: 42.250253,
+        lng: -83.609981
+    }
+}, {
+    title: "Hopcat",
+    location: {
+        lat: 42.278930,
+        lng: -83.741635
+    }
+}, {
+    title: "Salt Springs Brewery",
+    location: {
+        lat: 42.165879,
+        lng: -83.780447
+    }
+}, {
+    title: "Batch Brewing Company",
+    location: {
+        lat: 42.328527,
+        lng: -83.063352
+    }
+}, {
+    title: "Witch's Hat Brewing ",
+    location: {
+        lat: 42.454514,
+        lng: -83.650345
+    }
+}]
