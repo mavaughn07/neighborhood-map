6ac3f56add87ac6bfe0b0b131bf62ce994f5696f
Maxwell Vaughn <mavaughn07@gmail.com>
2018-11-22 09:50:45 -0500

    renamed file, added breweries, and added placeholder for position

---
+++ models.js
@@ -0,0 +1,129 @@
+Views = {
+    var bars = [{
+        name: "Canton Brew Works",
+        position: {
+            lat: ,
+            lng:
+        }
+    }, {
+        name: "Liberty Street Brewing",
+        position: {
+            lat: ,
+            lng:
+        }
+    }, {
+        name: "ROAK",
+        position: {
+            lat: ,
+            lng:
+        }
+    }, {
+        name: "Rochester Mills",
+        position: {
+            lat: ,
+            lng:
+        }
+    }, {
+        name: "Brew Detroit",
+        position: {
+            lat: ,
+            lng:
+        }
+    }, {
+        name: "The Detroit Beer Co",
+        position: {
+            lat: ,
+            lng:
+        }
+    }, {
+        name: "Atwater",
+        position: {
+            lat: ,
+            lng:
+        }
+    }, {
+        name: "Motor City Brewing",
+        position: {
+            lat: ,
+            lng:
+        }
+    }, {
+        name: "Blue Tractor",
+        position: {
+            lat: ,
+            lng:
+        }
+    }, {
+        name: "Jolly Pumpkin",
+        position: {
+            lat: ,
+            lng:
+        }
+    } {
+        name: "Arbor Brewing",
+        position: {
+            lat: ,
+            lng:
+        }
+    }, {
+        name: "Glass House",
+        position: {
+            lat: ,
+            lng:
+        }
+    }, {
+        name: "Wolverine",
+        position: {
+            lat: ,
+            lng:
+        }
+    }, {
+        name: "Homes",
+        position: {
+            lat: ,
+            lng:
+        }
+    }, {
+        name: "Grizzly Peak",
+        position: {
+            lat: ,
+            lng:
+        }
+    }, {
+        name: "Stony Lake",
+        position: {
+            lat: ,
+            lng:
+        }
+    }, {
+        name: "The Corner Brewery",
+        position: {
+            lat: ,
+            lng:
+        }
+    }, {
+        name: "Hopcat",
+        position: {
+            lat: ,
+            lng:
+        }
+    }, {
+        name: "Salt Springs",
+        position: {
+            lat: ,
+            lng:
+        }
+    }, {
+        name: "Batch",
+        position: {
+            lat: ,
+            lng:
+        }
+    }, {
+        name: "Witch's Hat",
+        position: {
+            lat: ,
+            lng:
+        }
+    }]
+}
