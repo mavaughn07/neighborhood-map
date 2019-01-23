6b51b5efc876c62f009820f29852a9b99d29e864
Max <mavaughn07@gmail.com>
2019-01-23 14:13:45 -0500

    added more comments

--- js/controller.js
+++ js/controller.js
@@ -28,7 +28,7 @@ var viewModel = function() {
 
     this.itemFilter = ko.observable('');
 
-
+    //create a marker for each location in the locations array located in models.js
     for (var i = 0; i < locations.length; i++) {
         var position = locations[i].location;
         var title = locations[i].title;
@@ -42,11 +42,13 @@ var viewModel = function() {
             map: map
         });
 
+        //add default values if there is no data returned from foursquare
         marker.address = '';
         marker.phone = '';
         marker.website = '';
         marker.rating = '';
 
+        //ajax call to get data from foursquare api
         (function(marker) {
             $.ajax({
                 url: 'https://api.foursquare.com/v2/venues/search?ll=' + marker.position.lat() + ',' + marker.position.lng() + '&query=' + marker.title + '&client_id=MCLI5B3BQXDAOANZ0KROJJDTU1ENKQUMDZEXSEQ3VZZL3K5O&client_secret=ZFNPE0JKM3UIC3WCQBZ4UP0ZM4FWY42AQISP0Y2Q0Y2GU31L&v=20181125',
@@ -97,7 +99,10 @@ var viewModel = function() {
             })
         })(marker)
 
+        //add marker to markers location array
         self.markers.push(marker);
+
+        //listener to open infowindow on individual marker
         marker.addListener('click', function() {
             populateInfoWindow(this, infowindow);
         });
@@ -107,7 +112,6 @@ var viewModel = function() {
     map.fitBounds(bounds);
 
     // Extend the boundaries of the map for each marker and display the marker
-
     self.showListings = function() {
 
         for (var i = 0; i < self.markers().length; i++) {
@@ -120,12 +124,14 @@ var viewModel = function() {
         map.fitBounds(bounds);
     }
 
+    //highlights marker on mouseover 
     self.highlightMarker = function(marker) {
 
         marker.setIcon(highlightedIcon);
 
     }
 
+    //returns marker to default icon color
     self.defaultMarker = function(marker) {
 
         marker.setIcon(defaultIcon);
@@ -148,6 +154,7 @@ var viewModel = function() {
     }
 
     //pulled from previous Udacity lessons
+    //sets color and shape of marker
     function makeMarkerIcon(markerColor) {
         var markerImage = new google.maps.MarkerImage(
             'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|' + markerColor +
@@ -159,6 +166,8 @@ var viewModel = function() {
         return markerImage;
     }
 
+
+    //sets the data inside an opened infowindow
     function populateInfoWindow(marker, infowindow) {
         if (infowindow.marker != marker) {
             // Clear the infowindow content to give the streetview time to load.
@@ -177,18 +186,23 @@ var viewModel = function() {
         infowindow.open(map, marker);
     }
 
+    //toggles the hamburger icon and sidebar the width of the sidebar
     self.toggleMenu = function() {
         $(".sidebar").toggleClass("sidebar-toggle");
 
 
     }
 
+    //live search set on keydown
     this.breweryFilter = ko.computed(function() {
+        //takes value in search bar, all lower case and compares it to markers array
         var search = self.itemFilter().toLowerCase();
+        //if there is no data in the search bar set all markers to visible, and display all locations in the sidebar
         if (!search) {
             self.markers().forEach(marker => { marker.setVisible(true) });
             return self.markers();
         } else {
+            //display only valid results and set others as non visible
             return ko.utils.arrayFilter(self.markers(), function(marker) {
                 var filter = marker.title.toLowerCase().indexOf(search) > -1;
                 if (filter) {
